ee0eb5175b0db11069b450398b96838ffbb55a13
Cristóbal Veas <cristobal.veas@proton.me>
2024-10-30 12:29:47 -0300

    Corregir error de tipeo en llamada a base de datos en endpoint registros

--- src/app/registros/route.js
+++ src/app/registros/route.js
@@ -12,7 +12,7 @@ export async function GET(request) {
                 usuario,
                 fecha,
                 metadatos,
-                accion, 
+                accion 
             FROM registro;
         `;
 
@@ -36,7 +36,7 @@ export async function GET(request) {
                 usuario,
                 fecha,
                 metadatos,
-                accion, 
+                accion
             FROM registro
             WHERE id = ${id};
         `;
