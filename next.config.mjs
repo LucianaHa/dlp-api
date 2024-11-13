fd4842d04aa62486e08fdcf861d00cd9294502e5
Cristóbal Veas <cristobal.veas@proton.me>
2024-11-13 12:28:17 -0300

    Configurar CORS para aceptar solicitudes desde dominios dlp

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://dlp-admin.vercel.app, https://dlp-prestamo.vercel.app, https://dlp-donacion.vercel.app, https://dlp-dashboard.vercel.app, https://localhost:3000/, http://localhost:3001/" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
+}
 
 export default nextConfig;
