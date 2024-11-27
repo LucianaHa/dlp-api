5032c82ca39ffe17cfc15e181bb1c6007c56ab72
Cristóbal Veas <cristobal.veas@proton.me>
2024-11-27 13:41:34 -0300

    Configurar Next.js para que no almacene las respuestas en cache

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Origin", value: "https://dlp-admin-dlp.vercel.app, https://dlp-prestamo.vercel.app, https://dlp-donacion.vercel.app, https://dlp-dashboard.vercel.app, http://localhost:3000/, https://localhost:3000/, http://localhost:3001/, https://localhost:3000/" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Cache-Control", value: "no-store" }
                 ]
             }
         ]
